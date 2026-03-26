f874df351c1b8ac20ff247ec8a8012ef1b07ee96
VladikNV250 <vladiknemichev@gmail.com>
2026-03-26 18:53:49 +0200

    feat(SN-11): map frequencies to musical notes, octaves and cents

---
+++ client/src/core/tuning/types.ts
@@ -0,0 +1,7 @@
+export type Note = 'C' | 'C#' | 'D' | 'D#' | 'E' | 'F' | 'F#' | 'G' | 'G#' | 'A' | 'A#' | 'B'
+
+export interface PitchData {
+    note: Note
+    octave: number
+    cents: number
+}
