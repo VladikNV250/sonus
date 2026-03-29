28eaa705a2235ecb0598a655135ee1fd3ab31c95
VladikNV250 <vladiknemichev@gmail.com>
2026-03-29 22:49:05 +0300

    fix(SN-12): set default Button type to 'button'

--- client/src/shared/ui/Button/Button.tsx
+++ client/src/shared/ui/Button/Button.tsx
@@ -7,10 +7,11 @@ import { type ButtonVariants, buttonVariants } from './variants'
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, ButtonVariants {}
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-    ({ className, variant, size, ...props }, ref) => {
+    ({ className, variant, type = 'button', size, ...props }, ref) => {
         return (
             <button
                 ref={ref}
+                type={type}
                 className={cn(buttonVariants({ variant, size, className }))}
                 {...props}
             />
