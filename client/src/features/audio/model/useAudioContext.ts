4b4f1e88666a61a8c365be212291c45af38dcf6a
VladikNV250 <vladiknemichev@gmail.com>
2026-03-29 22:46:30 +0300

    fix(SN-12): fix typo

--- client/src/features/audio/model/useAudioContext.ts
+++ client/src/features/audio/model/useAudioContext.ts
@@ -6,7 +6,7 @@ export const useAudioContext = () => {
     const context = useContext(AudioContext)
 
     if (!context) {
-        throw new Error('useAudio must be used within an AudioProvider')
+        throw new Error('useAudioContext must be used within an AudioProvider')
     }
 
     return context
