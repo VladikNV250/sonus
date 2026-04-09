0def287e0327e02f8d6166b4479b18029bbd4731
VladikNV250 <vladiknemichev@gmail.com>
2026-04-09 16:33:21 +0300

    fix(SN-6): don't precache audio-processor.js

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -19,8 +19,6 @@ export default defineConfig({
             workbox: {
                 globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
 
-                additionalManifestEntries: [{ url: '/audio-processor.js', revision: null }],
-
                 runtimeCaching: [
                     {
                         urlPattern: /\/api\//,
