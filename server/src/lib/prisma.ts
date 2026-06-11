b5d8ccefbcbabbd8ffa5217450e282c0017728b0
VladikNV250 <vladiknemichev@gmail.com>
2026-06-12 00:19:07 +0300

    fix: check database url before connection to prisma client

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -2,7 +2,11 @@ import 'dotenv/config'
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 import { PrismaClient } from '../../generated/prisma/client'
 
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = process.env.DATABASE_URL
+
+if (!connectionString) {
+    throw new Error('DATABASE_URL environment variable is not set')
+}
 
 const adapter = new PrismaBetterSqlite3({ url: connectionString })
 const prisma = new PrismaClient({ adapter })
