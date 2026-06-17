91466fff88d2162bd17ee372cecc2075837a1c69
VladikNV250 <vladiknemichev@gmail.com>
2026-06-17 20:46:29 +0300

    fix(SN-31): remove initial state for theme provider context

--- client/src/shared/lib/theme/context.ts
+++ client/src/shared/lib/theme/context.ts
@@ -2,9 +2,4 @@ import { createContext } from 'react'
 
 import type { ThemeProviderState } from './types'
 
-const initialState: ThemeProviderState = {
-    theme: 'dark',
-    setTheme: () => null,
-}
-
-export const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
+export const ThemeProviderContext = createContext<ThemeProviderState | undefined>(undefined)
